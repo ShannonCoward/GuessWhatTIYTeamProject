43e7ffcf91bb508dffc5b693e7d834c49f5a408f
ShannonArmon <shannon.armon@gmail.com>
2015-06-22 15:01:27 -0400

    minor adds

--- GuessWhatTIYTeamProject/GuessViewController.swift
+++ GuessWhatTIYTeamProject/GuessViewController.swift
@@ -8,21 +8,31 @@
 
 import UIKit
 
-class GuessViewController: UIViewController {
+class GuessViewController: UIViewController{
     
     @IBOutlet weak var pointsLabel: UILabel!
     
-    @IBAction func makeAGuessButton(sender: UIButton) {
-    }
+   
     
     @IBOutlet weak var gamePhotoView: UIImageView!
+    
+    @IBOutlet weak var guessTextField: UITextField!
+    
 
     override func viewDidLoad() {
+        
+        
+
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tryAGuess(sender: AnyObject) {
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
